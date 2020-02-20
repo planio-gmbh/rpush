d697ade4de0347157e69f8ad1f3de618592b206e
Jens Kraemer <jk@jkraemer.net>
2020-02-20 11:16:59 +0800

    support for webpush with VAPID

---
+++ lib/rpush/client/active_record/webpush/app.rb
@@ -0,0 +1,13 @@
+module Rpush
+  module Client
+    module ActiveRecord
+      module Webpush
+        class App < Rpush::Client::ActiveRecord::App
+          include Rpush::Client::ActiveModel::Webpush::App
+        end
+      end
+    end
+  end
+end
+
+
